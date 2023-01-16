45b1f280fbbb17b393af72048408b95e6a9ce1ae
jhunzkie <122717842+jhunzkiemanuel@users.noreply.github.com>
2023-01-16 17:42:49 +0800

    Add files via upload
    basic activity on csharp

---
+++ jhuzkie/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace jhunzkie
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("enter any name: ");
+            string name = Console.ReadLine().ToUpper();
+            char[] charN = name.ToCharArray();
+
+            for (int a = 0; a <=(charN.Length - 1)/2; a++)
+            {
+                for (int spc = 0; spc <= a; spc++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int b = a; b <= (charN.Length - 1) - a; b++)
+                {
+                    Console.Write(charN[b]);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
